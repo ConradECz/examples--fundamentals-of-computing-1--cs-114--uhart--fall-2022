a8d3f0831e5917d3ccbdfd1628fe7fb1392570d3
ConradECz <conradec21@gmail.com>
2022-10-24 08:12:19 -0400

    Update ASingleForLoop.java
    while loop added

--- in-class-examples/repetitive-structures/the-for-loop/ASingleForLoop.java
+++ in-class-examples/repetitive-structures/the-for-loop/ASingleForLoop.java
@@ -10,6 +10,22 @@ public class ASingleForLoop {
             }
         }
 
+
+        int i = 0;
+        int j = 0;
+        while(i < 4) {
+
+            int j = 0;
+            while(j < 4) {
+            System.out.println("i is " + i + " and j is " + j);
+
+                j++;
+
+            }
+            j = 0;
+
+            i++;
+        }
         // for(int i = 0; i < 10; ) {
         //     System.out.println("The value of i is " + i);
 
